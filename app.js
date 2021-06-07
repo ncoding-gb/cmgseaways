b2f5eab4574250431a5a6cedc316ce2945e674c8
ncoding-gb <80422700+ncoding-gb@users.noreply.github.com>
2021-06-07 20:53:36 +0400

    Update app.js

--- app.js
+++ app.js
@@ -92,3 +92,20 @@ closepopup.onclick = function () {
   document.body.style.overflow = "auto"; // ADD THIS LINE
   document.body.style.height = "auto"; // ADD THIS LINE
 };
+// fb
+
+
+  window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
